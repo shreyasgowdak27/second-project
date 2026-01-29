48d05e01a31664985bba218f17ad38e4e339e50e
shreyasgowdak27 <gshreyas151@gmail.com>
2026-01-29 10:47:31 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+public class Demo {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a=12;
+        b=23;
+        System.out.println(a);
+        System.out.println(a+b);
+        System.out.println(b);
+    }
+
+    
+}
